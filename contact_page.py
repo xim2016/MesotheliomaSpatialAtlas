ed74b5e6ef4dd7e212fc3b1e5e21d76043201f2b
xim2016 <xj_ma@yahoo.com>
2023-08-24 17:52:30 -0400

    updates

---
+++ contact_page.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from style import define_layout
+
+def contact_page():
+    
+    max_width = '90%'
+    padding_top = '2.5rem'
+    padding_right = '0rem'
+    padding_left = '10rem'
+    padding_bottom = '0rem'
+    define_layout(max_width, padding_top, padding_right, padding_left, padding_bottom)
+   
+    
+    a, b = st.columns([1, 1.6 ])
+    a.image('https://wexfordscitech.com/wp-content/uploads/2021/03/Assembly-web-5.png')
+    a.markdown("""<span style="font-size:20px;">University of Pittsburgh, UPMC Hillman Cancer Center, Assembly Building</span>""", unsafe_allow_html=True)
+
+    b.markdown("""<div style="width: 100%"><iframe width="100%" height="800" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=5051%20Centre%20Avenue%20Pittsburgh,%20PA%2015213+(My%20Business%20Name)&amp;t=p&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">measure distance on map</a></iframe></div>""", unsafe_allow_html=True)
+    # b.write('Hillman Cancer Center, 5051 Centre Avenue Pittsburgh, PA 15213') 
+
+   
+    # a.markdown('##### If you have any questions or feedback, please feel free to contact us:')
+    a.markdown("""<span style="font-size:18px;">Hatice Osmanbeyoglu<br>Principal Investigator<br>✉️ osmanbeyogluhu@pitt.edu</span>""", unsafe_allow_html=True)
+    a.markdown("""<span style="font-size:18px;">Xiaojun Ma<br>Senior Software Engineer<br>✉️ xim33@pitt.edu</span>""", unsafe_allow_html=True)
+ 
