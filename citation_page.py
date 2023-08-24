6507f67d4873515a9c59b0362c4b92f5cf91a50a
xim2016 <xj_ma@yahoo.com>
2023-08-24 17:52:14 -0400

    updates

---
+++ citation_page.py
@@ -0,0 +1,12 @@
+from style import define_layout
+
+def citation_page():
+    
+    max_width = '90%'
+    padding_top = '2.5rem'
+    padding_right = '0rem'
+    padding_left = '10rem'
+    padding_bottom = '0rem'
+    define_layout(max_width, padding_top, padding_right, padding_left, padding_bottom)
+   
+    
