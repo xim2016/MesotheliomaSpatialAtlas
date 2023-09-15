7d71db80a4fd48bbd2cb9e985c5a06671e763bc7
xim2016 <xj_ma@yahoo.com>
2023-09-14 21:31:09 -0400

    updates

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from utils import get_screen_width_height
-from Mobile_version.start import start_mobile
 from start import start
+from mobile.start import start_mobile
 
 st.set_page_config(
         layout='wide',
@@ -13,8 +13,8 @@ st.set_page_config(
 st.elements.utils._shown_default_value_warning=True
 
 width, height = get_screen_width_height()
-
-if width < 600 :  # suppose it is mobile device
+# st.write(width)
+if int(width) < 600 :  # suppose it is mobile device
     start_mobile()
 else:
     start()
