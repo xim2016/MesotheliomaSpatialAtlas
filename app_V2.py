b19f5cffe7b8374bcf4c226c7423770840c52ef9
xim2016 <xj_ma@yahoo.com>
2023-09-28 21:01:51 -0400

    Update app_V2.py

--- app_V2.py
+++ app_V2.py
@@ -1,20 +1,20 @@
 import streamlit as st
-from utils import get_screen_width_height
+from utils import get_screen_width
 from start_page import start_page
 from mobile.start_page_mobile import start_page_mobile
 
 st.set_page_config(
         layout='wide',
-        page_title='Mesothelioma Spacial Atlas',
+        page_title='Mesothelioma Spatial Atlas',
         page_icon="./assets/figures/meso_ribbon.png",
         # initial_sidebar_state="collapsed",
 )
 
 st.elements.utils._shown_default_value_warning=True
 
-width = get_screen_width_height()
+width = get_screen_width()
 
-mobile_device = int(width) < 600
+mobile_device = int(width) < 550
 
 if mobile_device :  #  mobile device detected
     start_page_mobile()
